4e976028adddf0c08b5ad6279e798062fadfbd58
rodrigoaraujosantos <araujo.1983@hotmail.com>
2022-10-13 17:51:40 -0300

    card e navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProdutoComponent } from './produto/produto.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
+  
   declarations: [
-    AppComponent
+    AppComponent,
+    ProdutoComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/produto/produto.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+/**
+ * todo componente do angular é representado por uma classe
+ * 
+ */
+@Component({
+  selector: 'card-produto',
+  templateUrl: './produto.component.html',
+  styleUrls: ['./produto.component.css']
+})
+export class ProdutoComponent {
+
+}
